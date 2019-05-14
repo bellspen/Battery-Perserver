a70b092c35ea694f38e579a1c355f37cb4839d90
Spencer Bell <spencerb3@live.ca>
2019-05-14 14:26:11 -0600

    Initial test works

---
+++ FirstTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace FirstTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //SYSTEM_POWER_STATUS Test;
+            //GetSystemPowerStatus(&Test);
+            
+
+
+        }
+    }
+}
